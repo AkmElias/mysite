29ec7ffc7aac45fc32e55ccf105edc80486ceae5
AKM Elias <akmelias11@gmail.com>
2022-10-19 07:26:11 +0600

    fabicon updates

--- js/main.js
+++ js/main.js
@@ -1,43 +1,49 @@
-$(window).load(function(){
-	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+$(window).load(function () {
+  $("#preloader").fadeOut("slow", function () {
+    $(this).remove();
+  });
 });
-
 
 /******************************************************************************************************************************
 Learn More Page Scroll
 *******************************************************************************************************************************/
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function () {
+  $("a.page-scroll").bind("click", function (event) {
+    var $anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($anchor.attr("href")).offset().top,
+        },
+        1500,
+        "easeInOutExpo"
+      );
+    event.preventDefault();
+  });
 });
 
 /******************************************************************************************************************************
 Menu
-*******************************************************************************************************************************/ 
-(function() {
+*******************************************************************************************************************************/
+(function () {
+  var bodyEl = document.body,
+    //content = document.querySelector( '.content-wrap' ),
+    openbtn = document.getElementById("open-button"),
+    closebtn = document.getElementById("close-button"),
+    isOpen = true;
 
-	var bodyEl = document.body,
-		//content = document.querySelector( '.content-wrap' ),
-		openbtn = document.getElementById( 'open-button' ),
-		closebtn = document.getElementById( 'close-button' ),
-		isOpen = false;
+  function init() {
+    initEvents();
+  }
 
-	function init() {
-		initEvents();
-	}
+  function initEvents() {
+    openbtn.addEventListener("click", toggleMenu);
+    if (closebtn) {
+      closebtn.addEventListener("click", toggleMenu);
+    }
 
-	function initEvents() {
-		openbtn.addEventListener( 'click', toggleMenu );
-		if( closebtn ) {
-			closebtn.addEventListener( 'click', toggleMenu );
-		}
-
-		/* close the menu element if the target it´s not the menu element or one of its descendants..
+    /* close the menu element if the target it´s not the menu element or one of its descendants..
 		content.addEventListener( 'click', function(ev) {
 			var target = ev.target;
 			if( isOpen && target !== openbtn ) {
@@ -45,20 +51,16 @@ Menu
 			}
 		} );
 		*/
-	}
+  }
 
-	function toggleMenu() {
-		if( isOpen ) {
-			classie.remove( bodyEl, 'show-menu' );
-		}
-		else {
-			classie.add( bodyEl, 'show-menu' );
-		}
-		isOpen = !isOpen;
-	}
+  function toggleMenu() {
+    if (isOpen) {
+      classie.remove(bodyEl, "show-menu");
+    } else {
+      classie.add(bodyEl, "show-menu");
+    }
+    isOpen = !isOpen;
+  }
 
-	init();
-
+  init();
 })();
-
-
